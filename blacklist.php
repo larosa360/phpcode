bc38f857d7b5240bb5188aa5c693c6be6258d88b
larosa360 <larosa360@yahoo.com>
2017-06-08 23:57:51 -0500

    Update blacklist.php

--- blacklist.php
+++ blacklist.php
@@ -5,14 +5,13 @@ if(empty($_POST['input'])){
 //Blacklists
 $blacklist = ["dnsbl-1.uceprotect.net","dnsbl-2.uceprotect.net","dnsbl-3.uceprotect.net","dnsbl.dronebl.org","dnsbl.sorbs.net","zen.spamhaus.org","bl.spamcop.net","list.dsbl.org","sbl.spamhaus.org","xbl.spamhaus.org","relays.osirusoft.com"];
 //define variables
-$inputMasterArray = array();
-
+$inputMasterArray = [];
+$blackListMasterArray = [];
 //sanitize input and split it into an array
 $inputArray = array_filter(array_map('trim', preg_split("/\r?\n?\s/", $_POST['input'])));
-
 //Process inputArryay
 foreach($inputArray as $host) {
-    $inputSubArray = array();
+    $inputSubArray = [];
     $resultip = '';
     if(filter_var($host, FILTER_VALIDATE_IP)){
         $resultip = $host;
@@ -29,17 +28,40 @@ foreach($inputArray as $host) {
     }
 }
 
+print_r("<html>");
+print_r("this is the first module");
+print_r("<br />");
+print_r($inputMasterArray);
+print_r("<br />");
+print_r("<br />");
+print_r("this is the second module");
+print_r("<br />");
+
+
 foreach($inputMasterArray as $host){
-	foreach($host as $entry => $ip){
+	foreach($host as $userInput => $ip){
 		if($ip != False){
 			$reverseip = implode('.', array_reverse(explode('.',$ip))); 
-			print_r($reverseip);
+			$blackListSubArray = [];
+			foreach($blacklist as $blacklHost){
+				if(checkdnsrr($reverseip . "." . $blacklHost . ".", "A")){
+					array_push($blackListSubArray, "yes");
+				}
+				else{
+					array_push($blackListSubArray, "no");
+				}
+			}
+			$blacklistLabelArray = array($userInput => $blackListSubArray);
+			array_push($blackListMasterArray, $blacklistLabelArray );
 		}
 		else{
-			print_r("user entered: " . $entry . " but it failed to resolve");
+			print_r("user entered: " . $userInput . " but it failed to resolve");
 		}
 	}
 }
-//Prints the current output of the foreach loop
 
+
+print_r($blackListMasterArray);
+print_r("</html>");
+//Prints the current output of the foreach loop
 ?>
